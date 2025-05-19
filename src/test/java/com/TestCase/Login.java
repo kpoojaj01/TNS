5e6d1308855416a98525f9dd7dc830e0a6fc0444
Automation <you@example.com>
2025-05-19 23:47:05 +0530

    first commit

---
+++ src/test/java/com/TestCase/Login.java
@@ -0,0 +1,26 @@
+package com.TestCase;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	@Test
+	void testvalid()
+	{
+	  System.out.println("Test Valid");
+	}
+	@Test
+	void testInvalid()
+	{
+		System.out.println("Test InValid");
+	}
+	@Test
+	void testSymbol()
+	{
+	
+		System.out.println("Test Symbol");
+	}
+	
+	
+
+}
---
+++ src/test/java/com/TestCase/Register.java
@@ -0,0 +1,19 @@
+package com.TestCase;
+
+import org.testng.annotations.Test;
+
+public class Register {
+
+	@Test
+	void testRegisterdata()
+	{
+		System.out.println("Test Registerdata");
+	}
+	
+	@Test
+	void testData()
+	{
+		System.out.println("Test Data");
+	}
+	
+}
